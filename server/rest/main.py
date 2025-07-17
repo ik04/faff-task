7e8986bb089b6aab6084a1120a438c63cca77b2f
ik04 <ishaankhurana123@gmail.com>
2025-07-18 02:53:54 +0530

    feat: add cors to the server

--- server/rest/main.py
+++ server/rest/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import voice, ws
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://faff-task.vercel.app/","http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(voice.router)
 app.include_router(ws.router)
